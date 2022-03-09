d431c428c4827a21760d9670684e386e34f87022
ycvivi <1914373138@qq.com>
2022-03-09 13:08:59 +0800

    1

---
+++ myApplication1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace myApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        ///直接删除指定目录下的所有文件及文件夹(保留目录)
+        /// </summary>
+        /// <param name="strPath">文件夹路径</param>
+        /// <returns>执行结果</returns>
+
+        public static void DeleteDir(string file)
+        {
+            try
+            {
+                //去除文件夹和子文件的只读属性
+                //去除文件夹的只读属性
+                System.IO.DirectoryInfo fileInfo = new DirectoryInfo(file);
+                fileInfo.Attributes = FileAttributes.Normal & FileAttributes.Directory;
+
+                //去除文件的只读属性
+                System.IO.File.SetAttributes(file, System.IO.FileAttributes.Normal);
+
+                //判断文件夹是否还存在
+                if (Directory.Exists(file))
+                {
+
+                    foreach (string f in Directory.GetFileSystemEntries(file))
+                    {
+
+                        if (File.Exists(f))
+                        {
+                            //如果有子文件删除文件
+                            File.Delete(f);
+                            Console.WriteLine(f);
+                        }
+                        else
+                        {
+                            //循环递归删除子文件夹
+                            DeleteDir(f);
+                        }
+                    }
+                    //删除空文件夹
+                    Directory.Delete(file);
+
+                }
+
+            }
+            catch (Exception ex) // 异常处理
+            {
+                Console.WriteLine(ex.Message.ToString());// 异常信息
+            }
+
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            DeleteDir(@"D:\zhidiangit3.0");
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            textBox1.Text = path; //将获取到的完整路径赋值到textBox1
+        }
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+	     	//拖拽的文件路径
+            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            MessageBox.Show("接收到文件 {fileName}");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;//允许窗体Drop事件 注意默认是false(也就是不能进行拖文件进入操作）
+        }
+    }
+}
